6b37bfc96bf0de713ade0a7786be762c66616caf
bdisptn <budisptn13@gmail.com>
2026-04-24 08:58:38 +0700

    Percobaan 1 dan modifikasi pertanyaan

---
+++ Jobsheet9/StackTugasMahasiswa.java
@@ -0,0 +1,76 @@
+public class StackTugasMahasiswa {
+    Mahasiswa07[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa(int size){
+        this.size = size;
+        stack = new Mahasiswa07[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+             return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa07 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa07 pop() {
+        if (!isEmpty()) {
+            Mahasiswa07 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa07 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    public void print() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+
+    public Mahasiswa07 peekBottom() {
+        if (!isEmpty()) {
+            return stack[0]; 
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas.");
+            return null;
+        }
+    }
+
+    public int count() {
+        return top + 1;
+    }
+}
